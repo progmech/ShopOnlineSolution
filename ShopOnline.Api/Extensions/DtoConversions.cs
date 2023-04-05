3e22f0b2bad9b791a5b16cb6f3cb01af2d3e52c3
Andrei <Andrei@PROGMECH-NB>
2023-04-05 20:31:44 +0300

    Add project files.

---
+++ ShopOnline.Api/Extensions/DtoConversions.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using ShopOnline.Api.Entities;
+using ShopOnline.Models.Dtos;
+
+namespace ShopOnline.Api.Extensions
+{
+    public static class DtoConversions
+    {
+        public static IEnumerable<ProductDto> ConvertToDto(
+            this IEnumerable<Product> products,
+            IEnumerable<ProductCategory> productCategories)
+        {
+            return products.Join(productCategories, product => product.CategoryId, category => category.Id, (product, category) =>
+            new ProductDto()
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                ImageURL = product.ImageURL,
+                Price = product.Price,
+                Qty = product.Qty,
+                CategoryId = category.Id,
+                CategoryName = category.Name
+            }).ToList();
+        }
+    }
+}
